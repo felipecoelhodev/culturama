9ef8eafb05907855664d0e86176cfe0f267f01de
felipecoelhodev <felipecoelhoflorenciotrab@gmail.com>
2026-05-16 10:57:48 -0300

    criando o projeto com Next.js 15

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-5xl font-extrabold tracking-tight sm:text-6xl">
+        Hello World!
+      </h1>
+    </div>
+  );
+}
